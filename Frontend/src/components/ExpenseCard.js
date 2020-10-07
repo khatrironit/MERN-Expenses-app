2f2002561a8397ad57f7956453e463691c01aca5
khatrironit <ronitkhatri1999@gmail.com>
2020-10-07 14:39:47 +0530

    added Edit and Remove buttons in ExpenseCard

--- Frontend/src/components/ExpenseCard.js
+++ Frontend/src/components/ExpenseCard.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper';
-
+import Paper from '@material-ui/core/Paper'
+import { FaPencilAlt } from 'react-icons/fa'
+import { FaTimesCircle } from 'react-icons/fa'
+import '../App.css'
 export default class ExpenseCard extends Component {
     render() {
         const { data } = this.props
         return (
-            <Grid item container xs = {11} spacing={0} justify="center">
-                <Grid item xs = {1}></Grid>
+            <Grid item container xs = {11} spacing={0} justify="center" alignItems="center">
+                <Grid item xs = {1} ><p className = "card-icons" onClick = {()=>console.log("clicked")}><FaPencilAlt /><br /> <small>Edit</small></p></Grid>
                 <Grid item xs = {9}>
                     <Paper className = "card-paper">
                         <Grid Container spacing={1}>
@@ -21,7 +23,7 @@ export default class ExpenseCard extends Component {
                         </Grid>
                     </Paper>
                 </Grid>
-                <Grid item xs = {1}></Grid>
+                <Grid item xs = {1} ><p className = "card-icons" onClick = {()=>console.log("clicked")}><FaTimesCircle /><br /> <small>Remove</small></p></Grid>
             </Grid>
         )
     }
